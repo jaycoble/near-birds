2e401863a465844fc349f7607c7674a0fd4a2eca
Jay <37637038+jaycoble@users.noreply.github.com>
2022-05-04 14:29:57 -0400

    update js and add additional files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
+
 
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
+module.exports.newBirdSearch = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode(
+        {
+            query: 'Unionville, NY',
+            limit: 1,
+        }
+    ).send()
+    console.log(geoData);
+    res.send('ok')
+}
+
+
+const localLat = ();
+const localLng = ();
